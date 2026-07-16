3d0b00c762276478e82113fc74339de8609a713a
Kishore <Kishorekamalesh77@gmail.com>
2026-07-16 17:45:43 +0530

    program created this java program using loops,arrays etc

---
+++ src/Day_2/Grade.java
@@ -0,0 +1,34 @@
+package Day_2;
+import java.util.Scanner;
+public class Grade {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter mark");
+        int mark=scan.nextInt();
+        if(mark<=100)
+        {
+            if(mark>90&&mark<=100)
+            {
+                System.out.println("grade A");
+            }
+            else if(mark>80&&mark<=90)
+            {
+                System.out.println("grade B");
+            }
+            else if(mark>64&&mark<=80)
+            {
+                System.out.println("grade C");
+            }
+            else
+            {
+                System.out.println("FAIL");
+            }
+        }
+        else
+        {
+            System.out.println("invalid input");
+        }
+
+    }
+}
