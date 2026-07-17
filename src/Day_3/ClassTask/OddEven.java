d871548527d597f7f8cc235e05b44b062b4d9dff
Kishore <Kishorekamalesh77@gmail.com>
2026-07-17 09:51:21 +0530

    program created this java program using loops,arrays etc

---
+++ src/Day_3/ClassTask/OddEven.java
@@ -0,0 +1,17 @@
+package Day_3.ClassTask;
+import java.util.Scanner;
+public class OddEven {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int a= sc.nextInt();
+
+        if(a%2==0){
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd number");
+        }
+        sc.close();
+    }
+}
---
+++ src/Day_3/HomeTask/Sample.java
@@ -0,0 +1,21 @@
+package Day_3.HomeTask;
+import java.util.Scanner;
+public class Sample {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int a=scanner.nextInt();
+
+        if(a>90){
+            System.out.println("greater than 90");
+        }
+        else if(a>50){
+            System.out.println("greater than 50");
+        }
+        else{
+            System.out.println("greater than 10");
+        }
+        scanner.close();
+    }
+}
+
