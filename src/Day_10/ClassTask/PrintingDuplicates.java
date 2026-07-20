2ad8fc349bcb86d184891994feb7c47bceb03de7
Kishore <Kishorekamalesh77@gmail.com>
2026-07-20 18:26:52 +0530

    new

---
+++ src/Day_10/ClassTask/PrintingDuplicates.java
@@ -0,0 +1,15 @@
+package Day_10.ClassTask;
+
+public class PrintingDuplicates {
+    public static void main(String[] args){
+        String [] arr = {"sam","Jane","sam","tom","sam"};
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr.length;j++){
+                if(arr[i].equals(arr[j])  && i<j){
+                    System.out.println(arr[j]);
+                    break;
+                }
+            }
+        }
+    }
+}
