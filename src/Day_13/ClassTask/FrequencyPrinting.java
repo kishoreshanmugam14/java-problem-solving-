4fc96b6fe2806e60262a006523491e02d47f6c9c
Kishore <Kishorekamalesh77@gmail.com>
2026-07-21 17:25:12 +0530

    practicing using various java statements

---
+++ src/Day_13/ClassTask/FrequencyPrinting.java
@@ -0,0 +1,21 @@
+package Day_13.ClassTask;
+
+public class FrequencyPrinting {
+    public static void main(String[] args) {
+        String name = "Good";
+
+        int count = 0;
+        for (int i = 0; i < name.length(); i++) {
+            for (int j = 0; j < name.length(); j++) {
+                if (name.charAt(i) == name.charAt(j)) {
+                    count++;
+                }
+            }
+            System.out.println(name.charAt(i) +": "+ count);
+            count =0;
+
+
+
+        }
+    }
+}
